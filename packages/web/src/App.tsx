f7c2e50e32b8ae8892b8ffdc0979e05510b1317b
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-28 09:12:49 -0300

    fix(web): moves homepage component to  folder

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { Home } from './components/Home';
+import { Home } from './pages/Home';
 import { GlobalStyles } from './components/Global';
 
 export const App = () => {
