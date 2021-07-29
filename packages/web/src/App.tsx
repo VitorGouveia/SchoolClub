f2d7b2a83b58755b2fdd7b76aee2dfc8d7dad8c1
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-29 15:32:31 -0300

    feat(web): make app a PWA

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,20 +1,23 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import { Theme } from "./contexts/ThemeContext"
+import { PWA } from "./pwa"
 
 import { Home } from './pages/Home';
 import { GlobalStyles } from './components/Global';
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
-        <GlobalStyles>
-          <Theme>
-            <Route path="/" component={() => <Home />} />
-          </Theme>
-        </GlobalStyles>
+        <PWA>
+          <GlobalStyles>
+            <Theme>
+              <Route path="/" component={() => <Home />} />
+            </Theme>
+          </GlobalStyles>
+        </PWA>
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
---
+++ packages/web/src/pwa/index.tsx
@@ -0,0 +1,16 @@
+import { FC, ReactNode } from "react"
+import { register } from "../serviceWorkerRegistration"
+
+type PWAProps = {
+  children: ReactNode
+}
+
+export const PWA: FC<PWAProps> = ({ children }) => {
+  register()
+
+  return (
+    <>
+    {children}
+    </>
+  )
+}
