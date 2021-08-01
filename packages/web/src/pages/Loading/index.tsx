614fe6d19dac7a5253e4427ca5a618b866dd592b
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-01 19:41:43 -0300

    feat(web): add loading screen component

--- packages/web/src/pages/Loading/index.tsx
+++ packages/web/src/pages/Loading/index.tsx
@@ -1,5 +1,54 @@
+import { Header, Post, Sidebar } from "../../components"
+
+import posts from "./posts.json"
+
+import "./styles.scss"
+
+const user = {
+  name: "Vitor Gouveia",
+  profile: "@vitorgouveia",
+  description: "Dev Sênior, Químico",
+  page: {
+    link: "vitor",
+    text: "See my articles!"
+  }
+}
+
+const ad = {
+  description: "The Ultimate Software Engineering Interview Prep",
+  phrase: "AlgoExpert | Ace the Coding Interviews",
+  page: {
+    link: "algo-expert",
+    text: "algoexpert.io"
+  }
+}
+
+type Tag = {
+  id: number
+  name: string
+}
+
+type Post = {
+  id: number
+  title: string
+  tags: Tag[]
+  image: string
+}
+
 export const Loading = () => {
   return (
-    <h1>loading...</h1>
+    <div className="loading">
+      <main className="homeContainer">
+        <nav>
+          <Header />
+        </nav>
+
+        <article>
+          {posts.map(post => <Post key={post.id} title={post.title} tags={post.tags} image={post.image} />)}
+        </article>
+
+        <Sidebar user={user} ad={ad} />
+      </main>
+    </div>
   )
 }
--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,2 +1,3 @@
 export { Home } from "./Home"
 export { Loading } from "./Loading"
+export { PostPage } from "./PostPage"
