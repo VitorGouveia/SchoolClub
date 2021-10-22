fe5ee86701e7b37f58e2f942a288a7405716e83a
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-10-22 20:38:08 -0300

    feat(web): add focus to links in `<Logo />` component

--- packages/web/src/components/Logo/styles.ts
+++ packages/web/src/components/Logo/styles.ts
@@ -1,5 +1,15 @@
 import styled, { css, keyframes } from "styled-components"
 
+const textFocusAnimation = keyframes`
+  from {
+    background-size: 100% 2px;
+  }
+  
+  to {
+    background-size: 100% 100%;
+  }
+`
+
 type LogoContainerProps = {
   direction?: "row" | "column"
 }
@@ -11,6 +21,39 @@ export const LogoContainer = styled.div<LogoContainerProps>`
   justify-content: center;
   
   flex-direction: ${props => props.direction};
+
+  [data-link="icon"] {
+    &:hover, &:focus {
+      #background {
+        fill: ${props => props.theme.colors.gray[700]};
+      }
+
+      #logo {
+        fill: ${props => props.theme.colors.accent[100]};
+      }
+    }
+  }
+
+  [data-link="title"] {
+    ${props => css`
+      background-image: linear-gradient(to right, ${props.theme.colors.accent[200]}, ${props.theme.colors.accent[100]});
+    `}
+    background-size: 0% 2px;
+    background-repeat: no-repeat;
+    background-position: left bottom;
+
+    transition: background 200ms;
+
+    &:hover {
+      cursor: pointer;
+      
+      background-size: 100% 2px;
+    }
+
+    &:focus {
+      animation: ${textFocusAnimation} forwards 200ms;
+    }
+  }
 `
 
 type SvgProps = {
@@ -18,22 +61,12 @@ type SvgProps = {
 }
 
 export const SvgLogo = styled.svg<SvgProps>`
-  margin-right: ${props => props.marginLeftClamp};
   background: ${props => props.theme.colors.gray[900]};
   cursor: pointer;
+  margin-right: ${props => props.marginLeftClamp};
 
   path {
     transition: all 200ms;  
-  }
-
-  &:hover {
-    #background {
-      fill: ${props => props.theme.colors.gray[700]};
-    }
-
-    #logo {
-      fill: ${props => props.theme.colors.accent[100]};
-    }
   }
 `
 
@@ -41,43 +74,14 @@ type LogoTitleProps = {
   fontSizeClamp: string
 }
 
-const textFocusAnimation = keyframes`
-  from {
-    background-size: 100% 2px;
-  }
-  
-  to {
-    background-size: 100% 100%;
-  }
-`
-
 export const LogoTitle = styled.h1<LogoTitleProps>`
   font-size: ${props => props.fontSizeClamp};
   font-family: ${props => props.theme.fonts.DM};
   font-weight: 400;
 
   height: min-content;
-
-  ${props => css`
-    background-image: linear-gradient(to right, ${props.theme.colors.accent[200]}, ${props.theme.colors.accent[100]});
-  `}
-  background-size: 0% 2px;
-  background-repeat: no-repeat;
-  background-position: left bottom;
-
-  transition: background 200ms;
   
   strong {
     font-weight: 700
-  }
-  
-  &:hover {
-    cursor: pointer;
-    
-    background-size: 100% 2px;
-  }
-
-  &:focus {
-    animation: ${textFocusAnimation} forwards 200ms;
   }
 `
