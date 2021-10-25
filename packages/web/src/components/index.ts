2b600dccad3cd141004391e20d7df5b3e6157b7f
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-10-25 05:59:17 -0300

    feat: add input component

--- packages/web/src/components/index.ts
+++ packages/web/src/components/index.ts
@@ -1,4 +1,6 @@
 export { default as Button } from "./Button"
+export { default as Checkbox } from "./Form/Checkbox"
+export { default as Input } from "./Form/Input"
 export { default as Header } from "./Header"
 export { default as Link } from "./Link"
 export { default as Logo } from "./Logo"
