48b757d913dae178abce1442cb9042b56b099974
VitorGouveia <vitorneves.gouveia10@gmail.com>
2021-12-29 22:10:48 -0300

    fix(server): add CORS to express ap

--- packages/server/src/@types/env.d.ts
+++ packages/server/src/@types/env.d.ts
@@ -1,9 +1,10 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
+    APP_URL: string;
     ACCESS_TOKEN_SECRET: string;
-    REFRESH_TOKEN_SECRET: string;
     ACCESS_TOKEN_EXPIRE: string;
+    REFRESH_TOKEN_SECRET: string;
     REFRESH_TOKEN_EXPIRE: string;
   }
 }
